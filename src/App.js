c4bd142fa6a10b072273e9cefa636d8efbd3c458
Eliezer <elimasyrubi@gmail.com>
2020-11-28 16:04:51 -0300

    Finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Formulario from "./Components/Formulario";
+import ListadoImagenes from "./Components/ListadoImagenes";
 
 function App() {
+  const [busqueda, guardarbusqueda] = useState("");
+  const [imagenes, guardarImagenes] = useState([]);
+  const [paginaactual, guardarPaginaActual] = useState(1);
+  const [totalpaginas, guardarTotalPaginas] = useState(1);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (busqueda === "") return;
+
+      const imagenesPorPaginas = 30;
+      const key = "8642621-4b8ebb75a79c72d8cdcfe6a39";
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per-page=${imagenesPorPaginas}&page=${paginaactual}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarImagenes(resultado.hits);
+
+      //calcular el total de las paginas
+      const calcularTotalPaginas = Math.ceil(
+        resultado.totalHits / imagenesPorPaginas
+      );
+
+      guardarTotalPaginas(calcularTotalPaginas);
+
+      //Mover la pantalla hacia arriba
+        const jumbotrom = document.querySelector('.jumbotron')
+        jumbotrom.scrollIntoView({behavior:'smooth'})
+
+    };
+    consultarAPI();
+  }, [busqueda,paginaactual]);
+
+  //definir la pagina anterior
+  const paginaAnterior = () => {
+    const nuevaPaginaActual = paginaactual - 1;
+    if (nuevaPaginaActual === 0) return;
+    guardarPaginaActual(nuevaPaginaActual);
+  };
+  //definir la pagina siguiente
+  const paginaSiguiente = () => {
+    const nuevaPaginaActual = paginaactual + 1;
+    if (nuevaPaginaActual > totalpaginas) return;
+    guardarPaginaActual(nuevaPaginaActual);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscado de imagenes</p>
+        <Formulario guardarbusqueda={guardarbusqueda} />
+      </div>
+      <div className="row justify-content-center">
+        <ListadoImagenes imagenes={imagenes} />
+
+        {(paginaactual === 1) ? null : (
+          <button
+          type="button"
+          className="bbtn btn-info mr-1"
+          onClick={paginaAnterior}
         >
-          Learn React
-        </a>
-      </header>
+          &laquo; Anterior{" "}
+        </button>
+        )}
+
+       {(paginaactual === totalpaginas) ? null : (
+          <button
+          type="button"
+          className="bbtn btn-info"
+          onClick={paginaSiguiente}
+        >
+          Siguiente &raquo;
+        </button>
+       )}
+      </div>
     </div>
   );
 }
